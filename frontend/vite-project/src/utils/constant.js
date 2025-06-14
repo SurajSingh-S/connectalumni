aca8032bec47cb92a31472635af92287ad11d09c
SurajSingh-S <146805274+SurajSingh-S@users.noreply.github.com>
2025-06-14 12:36:01 +0530

    Update constant.js

--- frontend/vite-project/src/utils/constant.js
+++ frontend/vite-project/src/utils/constant.js
@@ -1,3 +1,3 @@
-//export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
+export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
 //export const BASE_URL="https://connect-alumni-mern.vercel.app/";
-export const USER_API_END_POINT="https://connectalumnibackend.onrender.com";
+//export const USER_API_END_POINT="https://connectalumnibackend.onrender.com";
